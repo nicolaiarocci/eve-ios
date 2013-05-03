1d4dd96308c5898c8f1276d9e1f387606cd0368a
Nicola Iarocci <nicola@nicolaiarocci.com>
2013-05-03 17:32:28 +0200

    Test Suite [stub]

---
+++ Tests/Tests.cs
@@ -0,0 +1,29 @@
+
+using System;
+using NUnit.Framework;
+
+namespace Tests
+{
+	[TestFixture]
+	public class Tests
+	{
+		[Test]
+		public void Pass ()
+		{
+			Assert.True (true);
+		}
+
+		[Test]
+		public void Fail ()
+		{
+			Assert.False (true);
+		}
+
+		[Test]
+		[Ignore ("another time")]
+		public void Ignore ()
+		{
+			Assert.True (false);
+		}
+	}
+}
